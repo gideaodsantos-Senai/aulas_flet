561cf913d23917e8373938d52882414c049852ae
Gideão dos Santos <gideao.d.santos@aluno.senai.br>
2026-03-23 14:51:32 -0300

    atividade_Roberto

---
+++ atividadeFlet.py
@@ -0,0 +1,25 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+
+    def mostrar_mensagem(e):
+        page.add(
+            ft.Text("Pow! Pow!")
+        )
+
+    page.add(
+        ft.Text("Free Fire MAX"),
+        ft.Image(
+            src="images/ff.png",
+            width=150,
+            height=150
+        ),
+        ft.Button(
+            content="Famoso 3 capa ",
+            on_click=mostrar_mensagem
+        )
+    )
+
+
+ft.run(main)
